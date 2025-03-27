8fc93abde3cf39dd4355c1fa6a6862c408cb7f46
Nicholas <zhang.nicholas136@purdue.edu>
2025-03-27 18:39:48 -0400

    Updated Memory Spacing + Adding Gen Con Params

--- source/memory.sv
+++ source/memory.sv
@@ -4,12 +4,13 @@ module single_port_sync_ram
      parameter DEPTH = 16
     )
 
-  ( 	input 					clk,
-   		input [ADDR_WIDTH-1:0]	addr,
-   		inout [DATA_WIDTH-1:0]	data,
-   		input 					cs,
-   		input 					we,
-   		input 					oe
+  ( 	
+	input clk,
+   	input [ADDR_WIDTH-1:0]	addr,
+	inout [DATA_WIDTH-1:0]	data,
+   	input cs,
+   	input we,
+   	input oe
   );
 
   reg [DATA_WIDTH-1:0] 	tmp_data;
