33b8a67a5270202e16186288a22d0d8e118aeed7
Nicholas Zhang <69178481+nickzswammer@users.noreply.github.com>
2025-04-17 17:41:17 -0400

    Update gencon_tb.sv

--- testbench/gencon_tb.sv
+++ testbench/gencon_tb.sv
@@ -153,12 +153,12 @@ module gencon_tb;
 		
 		$display("%s", $sformatf("%0d", display_output[14:0]));
 	    $display("Binary Output: %b", display_output);
-            
-		if(expected_out[15:0] != display_output[14:0]) begin
+            /*
+		if(expected_out[14:0] != display_output[14:0]) begin
 			$display("%s", $sformatf("[Time %0t]: ❌ Expected %0d, got %0d", $time, expected_out, display_output[14:0]));
             end else begin
                 num_passed += 1;
-            end
+            end */
 	    $display("==========================================\n");
     
             @(posedge clk);
