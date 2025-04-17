cf7336bf1b17c2fd4bcb85fe7a36898ff626881a
Nicholas Zhang <69178481+nickzswammer@users.noreply.github.com>
2025-04-17 17:05:26 -0400

    Update gencon.sv

--- source/gencon.sv
+++ source/gencon.sv
@@ -189,6 +189,7 @@ module gencon (
             SHOW_RESULT_ALU: begin
                 complete <= 1;  // Indicate calculation done
                 display_output <= ALU_out;  // Store ALU result in display
+                $display("ALU_OUT: %b", ALU_out);
             end
 
             SHOW_RESULT_MULT: begin
