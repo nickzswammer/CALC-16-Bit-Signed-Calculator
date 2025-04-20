881264c0516d1fe00c856654278d6c1403b4b511
Nicholas Zhang <69178481+nickzswammer@users.noreply.github.com>
2025-04-20 16:56:58 -0400

    Update gencon_tb.sv

--- testbench/gencon_tb.sv
+++ testbench/gencon_tb.sv
@@ -172,6 +172,7 @@ module gencon_tb;
         test_number = 0;
         num_passed = 0;
 
+	    /*
 	// Addition tests (3'b001)
 	apply_inputs(2,   3'b001, 3, 5);            // 2 + 3 = 5
 	apply_inputs(1000,3'b001, 2345, 3345);      // 1000 + 2345 = 3345
@@ -211,6 +212,11 @@ module gencon_tb;
 	apply_inputs(-1,  3'b100, 1, -1);           // -1 * 1 = -1
 	apply_inputs(-1,  3'b100, -1, 1);           // -1 * -1 = 1
 
+	    */
+
+
+	// test case
+	    apply_inputs(11, 3'b001, 23, 34);
 
         $display("Passed %0d/%0d tests.\n", num_passed, test_number);
 	$display("==========================================\n");
