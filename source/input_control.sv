83bde8eb2a305a995a7a677fb93397de87b5104e
Nicholas Zhang <69178481+nickzswammer@users.noreply.github.com>
2025-05-01 18:15:31 -0400

    Update input_control.sv

--- source/input_control.sv
+++ source/input_control.sv
@@ -103,10 +103,8 @@ always_ff @(posedge Clock or negedge Reset) begin
             end
             CALCULATE: begin
                 // num 0s in data
-		 Sum <= !(Data[0] ^ Data[1] ^ Data[2] ^ Data[3] ^
-      	 	Data[4] ^ Data[5] ^ Data[6] ^ Data[7] ^
-     		  Data[8] ^ Data[9] ^ Data[10] ^ Data[11] ^
-      		 Data[12] ^ Data[13] ^ Data[14] ^ Data[15]);
+		Sum <= (Data != 16'hFFFF) && ((Data & (Data - 1)) == 0);
+
 		
                 State <= ANALYZE;                   
             end
