5ef23b44021ef42e253b9785506b6e57052c8ed8
Nicholas Zhang <69178481+nickzswammer@users.noreply.github.com>
2025-05-02 18:23:17 -0400

    Update input_control.sv

--- source/input_control.sv
+++ source/input_control.sv
@@ -51,7 +51,7 @@ module input_control (
 	
     // Sequential logic with active-low reset
     always_ff @(posedge clk or negedge nRST) begin
-	    $monitor("State: %d", state);
+	    $monitor("Input Control State: %d", state);
         if (!nRST) begin
             state <= IDLE;
             col_index <= 0;
