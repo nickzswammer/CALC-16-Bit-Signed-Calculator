418481b3da2ebaadff2967ec5f07352d83db1734
Nicholas Zhang <69178481+nickzswammer@users.noreply.github.com>
2025-05-02 15:24:55 -0400

    Update input_control.sv

--- source/input_control.sv
+++ source/input_control.sv
@@ -30,7 +30,6 @@
     // Sequential logic with active-low reset
     always_ff @(posedge clk or negedge nRST) begin
 	    $monitor("key_code: %h", key_code);
-			$monitor("Temp Key: %b", temp_key);
         if (!nRST) begin
             state <= IDLE;
             col_index <= 0;
@@ -53,6 +52,7 @@
     logic [3:0] temp_key;
     temp_key = encode_key(RowIn, col_index);
     key_code <= temp_key; // 🔁 always latch the key
+			$monitor("Temp Key: %b", temp_key);
 
     // ✅ Only raise read_input if it's a digit
     if (!read_input) begin
