ca64e5363584ce8f2ca57adf70bb807f299dec33
Nicholas Zhang <69178481+nickzswammer@users.noreply.github.com>
2025-05-02 15:18:22 -0400

    Update input_control_tb.sv

--- testbench/input_control_tb.sv
+++ testbench/input_control_tb.sv
@@ -52,16 +52,24 @@ module input_control_tb();
     @(posedge clk);
 
     // Now wait for the right output to be valid
+	  
     if (is_digit_key) begin
         wait (read_input);
 	$display("Key [%0d] => keypad = %0d",
               key_index, keypad_input);
-    end else begin
-        wait (operator_input != 3'b000 || equal_input);
-	$display("Key [%0d] => op = %0d, eq = %0b",
-              key_index, operator_input, equal_input);
+    end 
+    else begin
+	    if (key_index == 14)
+		$display("Empty Key");
+	    else begin
+	        wait (operator_input != 3'b000 || equal_input);
+		$display("Key [%0d] => op = %0d, eq = %0b",
+	              key_index, operator_input, equal_input);
+	    end
     end
-
+    
+	  
+	  
     key_read = 1;
     @(posedge clk);
     key_read = 0;
