cbe6b5744bc3ef74dc02695ed3268d5499ba2b2c
Nicholas Zhang <69178481+nickzswammer@users.noreply.github.com>
2025-05-02 21:03:23 -0400

    Update calculator_top_tb.sv

--- testbench/calculator_top_tb.sv
+++ testbench/calculator_top_tb.sv
@@ -38,13 +38,6 @@ module calculator_top_tb();
     wait(dut.input_ctrl_inst.state == 3);
     
     @(posedge clk);
-    @(posedge clk);    @(posedge clk);
-    @(posedge clk);    @(posedge clk);
-    @(posedge clk);    @(posedge clk);
-    @(posedge clk);    @(posedge clk);
-    @(posedge clk);    @(posedge clk);
-    @(posedge clk);    @(posedge clk);
-    @(posedge clk);    @(posedge clk);
     @(posedge clk);
 
     // Let gencon acknowledge
